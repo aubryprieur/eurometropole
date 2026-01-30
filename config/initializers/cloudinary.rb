f5372273ca33fc0755e0125f6c3e1fd96be91f22
aubry prieur <aubry.prieur@gmail.com>
2026-01-30 15:59:22 +0100

    Update Cloudinary config for direct uploads

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -2,3 +2,18 @@ Cloudinary.config do |config|
   config.secure = true
   config.cdn_subdomain = true
 end
+
+ActiveSupport.on_load(:active_storage_blob) do
+  ActiveStorage::Blob.class_eval do
+    def service_url_for_direct_upload(expires_in: 5.minutes)
+      Cloudinary::Utils.cloudinary_api_url("upload",
+        resource_type: "auto",
+        type: "upload"
+      ) + "?upload_preset=#{ENV['CLOUDINARY_UPLOAD_PRESET']}"
+    end
+
+    def service_headers_for_direct_upload
+      { "Content-Type" => content_type, "X-Unique-Upload-Id" => key }
+    end
+  end
+end

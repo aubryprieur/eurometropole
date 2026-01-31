beb44cbf3c464088561c95afdcf3ad2e46a51952
aubry prieur <aubry.prieur@gmail.com>
2026-01-31 18:20:06 +0100

    Configure CSP for S3 direct uploads

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -1,7 +1,9 @@
 Decidim.configure do |config|
   config.direct_upload = false
 
-  config.content_security_policy do |policy|
-    policy.connect_src :self, "*.amazonaws.com", "*.hereapi.com", :data
-  end
+  config.content_security_policies_extra = {
+    "connect-src" => %w(https://eurometropole-decidim.s3.eu-west-3.amazonaws.com),
+    "img-src" => %w(https://eurometropole-decidim.s3.eu-west-3.amazonaws.com),
+    "media-src" => %w(https://eurometropole-decidim.s3.eu-west-3.amazonaws.com)
+  }
 end

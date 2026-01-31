708f9d63f718a22b8b08d2bc9cd9cf1cb9749970
aubry prieur <aubry.prieur@gmail.com>
2026-01-31 18:14:43 +0100

    Add S3 to Decidim CSP config

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -1,3 +1,7 @@
 Decidim.configure do |config|
   config.direct_upload = false
+
+  config.content_security_policy do |policy|
+    policy.connect_src :self, "*.amazonaws.com", "*.hereapi.com", :data
+  end
 end

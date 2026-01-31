dfae04ff8bf7fab194ccc68b1bca319de0b43a6f
aubry prieur <aubry.prieur@gmail.com>
2026-01-31 18:07:56 +0100

    Add S3 to Content Security Policy

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,2 +1,6 @@
 # For tuning the Content Security Policy, check the Decidim documentation site
 # https://docs.decidim.org/en/develop/customize/content_security_policy
+
+Rails.application.config.content_security_policy do |policy|
+  policy.connect_src :self, "*.amazonaws.com", "*.hereapi.com", :data
+end

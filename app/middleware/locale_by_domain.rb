2aa062b3295a881fb97f4ec19375ca42efcf2e16
aubry prieur <aubry.prieur@gmail.com>
2026-03-03 19:55:34 +0100

    Redirect domain to primary host with locale parameter

--- app/middleware/locale_by_domain.rb
+++ app/middleware/locale_by_domain.rb
@@ -4,6 +4,8 @@ class LocaleByDomain
     'www.leie-eiland.eu' => 'nl'
   }.freeze
 
+  PRIMARY_HOST = 'eurometropole-3e673cb05ad0.herokuapp.com'
+
   def initialize(app)
     @app = app
   end
@@ -11,7 +13,10 @@ class LocaleByDomain
   def call(env)
     request = Rack::Request.new(env)
     if (locale = DOMAIN_LOCALES[request.host])
-      env['QUERY_STRING'] = "locale=#{locale}&#{env['QUERY_STRING']}"
+      query = "locale=#{locale}"
+      query += "&#{request.query_string}" unless request.query_string.empty?
+      location = "https://#{PRIMARY_HOST}#{request.path}?#{query}"
+      return [302, { 'Location' => location, 'Content-Type' => 'text/html' }, []]
     end
     @app.call(env)
   end

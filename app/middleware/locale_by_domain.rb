7bc914bf2ce5c072d591bffbe0522cecb390bb93
aubry prieur <aubry.prieur@gmail.com>
2026-03-03 20:16:15 +0100

    Fix middleware: ilot-lys.eu is now primary host

--- app/middleware/locale_by_domain.rb
+++ app/middleware/locale_by_domain.rb
@@ -1,10 +1,6 @@
 class LocaleByDomain
-  DOMAIN_LOCALES = {
-    'www.ilot-lys.eu'    => 'fr',
-    'www.leie-eiland.eu' => 'nl'
-  }.freeze
-
-  PRIMARY_HOST = 'eurometropole-3e673cb05ad0.herokuapp.com'
+  PRIMARY_HOST = 'www.ilot-lys.eu'
+  NL_HOST = 'www.leie-eiland.eu'
 
   def initialize(app)
     @app = app
@@ -12,12 +8,14 @@ class LocaleByDomain
 
   def call(env)
     request = Rack::Request.new(env)
-    if (locale = DOMAIN_LOCALES[request.host])
-      query = "locale=#{locale}"
+
+    if request.host == NL_HOST
+      query = "locale=nl"
       query += "&#{request.query_string}" unless request.query_string.empty?
       location = "https://#{PRIMARY_HOST}#{request.path}?#{query}"
       return [302, { 'Location' => location, 'Content-Type' => 'text/html' }, []]
     end
+
     @app.call(env)
   end
 end

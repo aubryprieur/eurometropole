91c96c9b38546bb6885ad51463353e17552c67bd
aubry prieur <aubry.prieur@gmail.com>
2026-03-10 22:13:59 +0100

    Clean up CSP initializer: frame-src now managed via Decidim organization settings

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,9 +1,6 @@
 # For tuning the Content Security Policy, check the Decidim documentation site
 # https://docs.decidim.org/en/develop/customize/content_security_policy
 
-Rails.application.config.after_initialize do
-  Rails.application.config.content_security_policy do |policy|
-    policy.connect_src :self, "*.amazonaws.com", "*.hereapi.com", :data
-    policy.frame_src :self, "www.youtube-nocookie.com", "player.vimeo.com", "player.twitch.tv", "meet.jit.si", "https://umap.openstreetmap.fr"
-  end
+Rails.application.config.content_security_policy do |policy|
+  policy.connect_src :self, "*.amazonaws.com", "*.hereapi.com", :data
 end

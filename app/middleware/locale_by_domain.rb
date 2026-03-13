5947c0cc4f7534b2c5892809fdcfad2a257ea545
aubry prieur <aubry.prieur@gmail.com>
2026-03-13 14:20:26 +0100

    Rewrite response URLs for NL domain to keep visitors on leie-eiland.eu

--- app/middleware/locale_by_domain.rb
+++ app/middleware/locale_by_domain.rb
@@ -32,6 +32,19 @@ class LocaleByDomain
       headers['Location'] = headers['Location'].gsub(PRIMARY_HOST, NL_HOST)
     end
 
+    # Rewrite HTML body links so NL visitors stay on leie-eiland.eu
+    if original_host == NL_HOST && headers['Content-Type']&.include?('text/html')
+      body = +""
+      response.each { |chunk| body << chunk }
+      response.close if response.respond_to?(:close)
+
+      body.gsub!("https://#{PRIMARY_HOST}", "https://#{NL_HOST}")
+      body.gsub!("http://#{PRIMARY_HOST}", "http://#{NL_HOST}")
+
+      headers['Content-Length'] = body.bytesize.to_s
+      response = [body]
+    end
+
     [status, headers, response]
   end
 end
